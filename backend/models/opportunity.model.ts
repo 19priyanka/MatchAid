6cd0fb34850bf27ad818164f93702c7ce60658f8
Anand Patel <anandpp2001@gmail.com>
2024-03-25 19:35:57 -0600

    fix: update getOpportunites function to use enum

--- backend/models/opportunity.model.ts
+++ backend/models/opportunity.model.ts
@@ -1,12 +1,13 @@
 import { OpportunityModel } from "../../src/models/opportunities";
 import { Opportunity, Status } from "../types/Opportunity";
+import { UserType } from "../types/User";
 
 const getOpportunities = async (
   email: string,
   userType: string
 ): Promise<Opportunity[]> => {
   let opportunities: Opportunity[] = [];
-  if (userType === "volunteer") {
+  if (userType === UserType.Volunteer) {
     opportunities = (await OpportunityModel.find({
       status: Status.Accepted, // Cast the status to the correct type
       time: { $gte: new Date() },
@@ -18,11 +19,11 @@ const getOpportunities = async (
             !opportunity.signedUpUsers.find((user) => user.email === email)
         );
       })) as Opportunity[];
-  } else if (userType === "organization") {
+  } else if (userType === UserType.Organization) {
     opportunities = (await OpportunityModel.find({
       "organization.email": email,
     }).exec()) as Opportunity[];
-  } else if (userType === "administrator") {
+  } else if (userType === UserType.Admin) {
     opportunities = (await OpportunityModel.find({}).exec()) as Opportunity[];
   } else {
     throw new Error("Invalid user type");
