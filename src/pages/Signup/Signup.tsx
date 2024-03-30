2817d476372e9a4547fa448ff6d72b1e109a3ee0
Priyanka Gautam <priyankagautam@Priyankas-MacBook-Pro.local>
2024-03-29 23:56:19 -0600

    feat: Login button wiring in static

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -102,7 +102,7 @@ export default function Login() {
                 />
                 <TextInput
                   label="Phone Number"
-                  placeholder="you@mantine.dev"
+                  placeholder="123-456-7890"
                   required
                   style={{  width: "88%", borderRadius: 15 }}
                   mt="lg"
