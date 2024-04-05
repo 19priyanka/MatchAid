362ae5d5398162ad630b6518a1c05ae4336dc81f
Anand Patel <anandpp2001@gmail.com>
2024-04-05 10:34:54 -0600

    fix: rebase fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,173 @@
 import type { GetServerSideProps, NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/shared/layout";
 import SearchInput from "../components/SearchBar/SearchInput";
 import { UserType } from "../CustomTypes/UserType";
+import VolunteerEventCard from "../components/UserCards/VolunteerEventCard";
 import { Group } from "@mantine/core";
 import { getSession } from "next-auth/react";
-import VolunteerEventCard from "../components/UserCards/VolunteerEventCard";
 
 export default function homePage() {
-  const [user, setUser] = useState("GUEST" as UserType);
-  const volunteerEvents = [
+  const [tabs, settabs] = useState([]);
+  const [user, setUser] = useState(UserType.ADMIN);
+  const [currentTab, setCurrentTab] = useState(
+    user == UserType.GUEST || user == UserType.VOLUNTEER ? -1 : 0
+  );
+  const [volunteerEvents, setVolunteerEvents] = useState<
     {
-      Name: "Soup Kitchen",
-      VolCount: 4,
-      Date: "April 14",
-      times: "8:30am-12:00pm",
-      Description:
-        "This is a description of the event that's going to take place.",
-    },
-    {
-      Name: "Homeless Shelter",
-      VolCount: 10,
-      Date: "April 15",
-      times: "2:30pm-9:00pm",
-      Description:
-        "This is a description of the event that's going to take place.",
-    },
-  ];
+      Name: string;
+      VolCount: number;
+      Date: string;
+      times: string;
+      Description: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    let URL = []; // this will be a string with the URL of the endpoint we want to fetch from later instead of the array of data
+    switch (currentTab) {
+      case 0:
+        if (user == UserType.ADMIN) {
+          URL = [
+            {
+              Name: "Soup Kitchen ADMIN TAB 0",
+              VolCount: currentTab,
+              Date: "April 14",
+              times: "8:30am-12:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+            {
+              Name: "Homeless Shelter",
+              VolCount: 10,
+              Date: "April 15",
+              times: "2:30pm-9:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+          ];
+        } else if (user == UserType.ORGANIZATION) {
+          URL = [
+            {
+              Name: "Soup Kitchen ORG TAB 0",
+              VolCount: currentTab,
+              Date: "April 14",
+              times: "8:30am-12:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+            {
+              Name: "Homeless Shelter",
+              VolCount: 10,
+              Date: "April 15",
+              times: "2:30pm-9:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+          ];
+        }
+
+        break;
+      case 1:
+        if (user == UserType.ADMIN) {
+          URL = [
+            {
+              Name: "Soup Kitchen ADMIN TAB 1",
+              VolCount: currentTab,
+              Date: "April 14",
+              times: "8:30am-12:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+            {
+              Name: "Homeless Shelter",
+              VolCount: 10,
+              Date: "April 15",
+              times: "2:30pm-9:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+          ];
+        } else if (user == UserType.ORGANIZATION) {
+          URL = [
+            {
+              Name: "Soup Kitchen ORG TAB 1",
+              VolCount: currentTab,
+              Date: "April 14",
+              times: "8:30am-12:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+            {
+              Name: "Homeless Shelter",
+              VolCount: 10,
+              Date: "April 15",
+              times: "2:30pm-9:00pm",
+              Description:
+                "This is a description of the event that's going to take place.",
+            },
+          ];
+        }
+        break;
+      case 2:
+        URL = [
+          {
+            Name: "Soup Kitchen ADMIN TAB 2",
+            VolCount: currentTab,
+            Date: "April 14",
+            times: "8:30am-12:00pm",
+            Description:
+              "This is a description of the event that's going to take place.",
+          },
+          {
+            Name: "Homeless Shelter",
+            VolCount: 10,
+            Date: "April 15",
+            times: "2:30pm-9:00pm",
+            Description:
+              "This is a description of the event that's going to take place.",
+          },
+        ];
+        break;
+      default:
+        URL = [
+          {
+            Name: "Soup Kitchen GENERAL TAB -1",
+            VolCount: currentTab,
+            Date: "April 14",
+            times: "8:30am-12:00pm",
+            Description:
+              "This is a description of the event that's going to take place.",
+          },
+          {
+            Name: "Homeless Shelter",
+            VolCount: 10,
+            Date: "April 15",
+            times: "2:30pm-9:00pm",
+            Description:
+              "This is a description of the event that's going to take place.",
+          },
+        ];
+        break;
+    }
+
+    // fetchData(URL)
+    //   .then((data) => {
+    //     // Update volunteerEvents with the fetched data
+    setVolunteerEvents(URL); // change URL to data after connection
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching data:', error);
+    //   });
+  }, [currentTab]); // Dependency array ensures the effect runs when selectedTab changes
+
+  // Function to fetch data based on the selected tab
+  // const fetchData = async (URL) => {
+  //   // const response = await fetch(URL);
+  //   // const data = await response.json();
+  //   return URL;
+  // };
+
   const renderTabs = () => {
     switch (user) {
       case UserType.ADMIN:
@@ -40,7 +181,11 @@ export default function homePage() {
 
   return (
     <Layout>
-      <SearchInput selected={0} tabs={renderTabs()} />
+      <SearchInput
+        selected={currentTab}
+        tabs={renderTabs()}
+        setTab={setCurrentTab}
+      />
 
       <Group justify="space-evenly" style={{ margin: 25 }}>
         {volunteerEvents.map((event, index) => {
